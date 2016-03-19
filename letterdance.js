9a22e2432270fcdc6ca1d413422f6c44d1101278
= <nateparro2t@gmail.com>
2016-03-19 04:51:26 -0400

    update library

--- letterdance.js
+++ letterdance.js
@@ -1,5 +1,6 @@
 (function() {
-	var FRACTION_OF_SCROLL_THAT_IS_PAGE_TRANSITION = 0.7;
+	var FRACTION_OF_SCROLL_THAT_IS_PAGE_TRANSITION = 0.2;
+	var FIXED_TRANSITION_TIME = 0.33;
 	var ORIGINAL_CONTENT_OPACITY = 0;
 	
 	var TEXT_NODE = 3;
@@ -136,9 +137,11 @@
 					cloned.style.display = 'block';
 					cloned.style.left = '0px';
 					cloned.style.top = '0px';
-					setTimeout(function() {
-						cloned.style.transition = 'transform 0.05s linear'; // wait for initial layout before applying transition
-					}, 0)
+					(function(cloned) {
+						setTimeout(function() {
+							cloned.style.transition = 'transform ' + FIXED_TRANSITION_TIME + 's ease-in-out, opacity ' + FIXED_TRANSITION_TIME + 's ease-in-out'; // wait for initial layout before applying transition
+						}, 0)
+					})(cloned);
 					self.overlay.appendChild(cloned);
 					cloneLetterNodes.push(cloned);
 				}
@@ -161,6 +164,8 @@
 					if (cur.opacity == 0) {
 						cur.left = next.left;
 						cur.top = next.top;
+						cur.width = next.width;
+						cur.height = next.height;
 					}
 				}
 			}
@@ -172,6 +177,8 @@
 					if (next.opacity == 0) {
 						next.left = cur.left;
 						next.top = cur.top;
+						next.width = cur.width;
+						next.height = cur.height;
 					}
 				}
 			}
@@ -222,7 +229,7 @@
 				seenLetterCounts[letter] = seenCount + 1;
 				
 				if (seenCount >= (instances[letter] || []).length) {
-					positions.push({left: 0, top: 0, opacity: 0});
+					positions.push({left: 0, top: 0, opacity: 0, width: 0, height: 0});
 				} else {
 					var pos = getPos(instances[letter][seenCount]);
 					positions.push({left: pos.left - pageOffset.left, top: pos.top - pageOffset.top, width: pos.width, height: pos.height, opacity: 1});
@@ -239,10 +246,10 @@
 				var pos = positions[i];
 				// letterClone.style.left = pos.left + nodeOffset.left + 'px';
 				// letterClone.style.top = pos.top + nodeOffset.top + 'px';
-				var x = (pos.left + nodeOffset.left);
-				var y = (pos.top + nodeOffset.top);
+				var x = parseInt(pos.left + nodeOffset.left);
+				var y = parseInt(pos.top + nodeOffset.top);
 				var rotate = pos.rotation || 0;
-				letterClone.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, 0) rotate(' + rotate + 'deg)';
+				letterClone.style.transform = 'translate(' + x + 'px, ' + y + 'px) rotate(' + rotate + 'deg)';
 				letterClone.style.opacity = pos.opacity;
 				
 			}
@@ -283,16 +290,15 @@
 			return out;
 		}
 		
-		self.pageCenterYs = function() {
+		self.pageYs = function() {
 			return self.pages.map(function(page) {
-				var b = getPos(page);
-				return b.top + b.height / 2;
+				return getPos(page).top;
 			})
 		}
 		
 		self.layout = function() {			
-			var y = window.innerHeight/2 + window.pageYOffset;
-			var pageYs = self.pageCenterYs();
+			var y = window.pageYOffset;
+			var pageYs = self.pageYs();
 			var lastPageBefore = 0;
 			var firstPageAfter = null;
 			for (var i=0; i<pageYs.length; i++) {
@@ -308,6 +314,8 @@
 			var t = (y2 == y1) ? 0 : (y - y1) / (y2 - y1);
 			self.applyLetterPositions(self.interpolateLetterPositions(lastPageBefore, firstPageAfter, t));
 		}
+		
+		DEBUG_LETTERDANCE = self;
 		
 		self.invalidateLayout = function() {
 			delete self.letterPositions;
